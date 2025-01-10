2df5bd406d55fda8058ba4c0f12d8b60a4aba29a
yailama <adakopyan@gmail.com>
2025-01-10 21:00:37 +0700

    feat: opentelemetry initial setup

--- app/main.py
+++ app/main.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI
 from .api import webhooks
 from .database import engine
 from .models.database import Dialogue, SQLModel
+from .telemetry import setup_telemetry
 from . import config
 
 SQLModel.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Chatwoot AI Handler")
+setup_telemetry(app, engine)
 
 app.include_router(webhooks.router,prefix="/api/v1") 
